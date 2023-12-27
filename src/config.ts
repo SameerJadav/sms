96ad3020f8c634ba301876fe57bd1ae1174deeaa
SameerJadav <sameerjadav010@gmail.com>
2023-12-27 14:00:39 +0530

    chore: initial setup

---
+++ src/config.ts
@@ -0,0 +1,18 @@
+export const SITE = {
+  name: "Student Management System",
+  description:
+    "Feature-rich Student Management System(SMS). Performant. Open source.",
+  url:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000"
+      : `https://${process.env.VERCEL_URL}`,
+  author: {
+    name: "Sameer Jadav",
+    site: "https://sameerjadav.me",
+    twitterUserName: "@SameerJadav_",
+    twitter: "https://twitter.com/SameerJadav_",
+    github: "https://github.com/SameerJadav",
+    linkedin: "https://www.linkedin.com/in/sameerjadav",
+    mail: "mailto:sameerjdav001@gmail.com",
+  },
+};
